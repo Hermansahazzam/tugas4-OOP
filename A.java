9442e85fa7e3dd16e2ca820c9b9a3a5fb1224a79
Hermansahazzam <32549306+Hermansahazzam@users.noreply.github.com>
2017-11-06 21:08:15 +0700

    Add files via upload

---
+++ A.java
@@ -0,0 +1,10 @@
+public class A {
+	
+	public static void main(String[] args) {
+		int a = 20, b = 10;
+		CalculatorMain cal = new CalculatorMain ();
+		cal.pertambahan(a,b);
+		cal.pengurangan(a,b);
+		cal.perkalian(a,b);
+	}
+}
